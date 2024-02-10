08c6b3cc47612b15558b5b90238898a05bc7108c
admercs <adam.michael.erickson@gmail.com>
2024-02-09 16:26:53 -0800

    update docs action

--- scripts/build_docs.sh
+++ scripts/build_docs.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-SCRIPT_DIR="$(realpath ${BASH_SOURCE[0]})"
-pushd "$SCRIPT_DIR" > /dev/null
+#SCRIPT_DIR="$(realpath ${BASH_SOURCE[0]})"
+#pushd "$SCRIPT_DIR" > /dev/null
 
 cp README.md docs/
 sed -i 's/](docs\//](/g' docs/README.md
 mkdocs build
 
-popd > /dev/null
+#popd > /dev/null
