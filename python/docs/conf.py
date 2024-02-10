adb245d642aa9a99ff675cd8c9db8ea27592f773
admercs <adam.michael.erickson@gmail.com>
2024-02-09 17:23:51 -0800

    update docs

--- python/docs/conf.py
+++ python/docs/conf.py
@@ -175,7 +175,7 @@ html_theme_options = {
     "favicon": "../images/rune.svg",
     "icon": {
         "logo": "material/shield-airplane",
-        "repo": "fontawesome/regular/brands/git-alt",
+        "repo": "fontawesome/fab/git-alt",
         "edit": "material/pencil",
         "view": "material/eye",
     },
