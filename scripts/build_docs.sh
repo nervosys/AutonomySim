cf01a7ad32a559b91c71b673c28631087adbde98
admercs <adam.michael.erickson@gmail.com>
2024-02-09 16:31:55 -0800

    update docs action

--- scripts/build_docs.sh
+++ scripts/build_docs.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-#SCRIPT_DIR="$(realpath ${BASH_SOURCE[0]})"
-#pushd "$SCRIPT_DIR" > /dev/null
-
-cp README.md docs/
-sed -i 's/](docs\//](/g' docs/README.md
+cp ./README.md ./docs/
+sed -i 's/](docs\//](/g' ./docs/README.md
 mkdocs build
-
-#popd > /dev/null
