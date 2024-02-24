59a460007262f71be90d5ae0513f465e62390410
admercs <adam.michael.erickson@gmail.com>
2024-02-24 12:56:14 -0800

    update test build

--- scripts/cmd/build.cmd
+++ scripts/cmd/build.cmd
@@ -171,9 +171,11 @@ if not exist Unreal\Plugins\AutonomySim\Content\VehicleAdv (
 )
 
 if not exist Unreal\Plugins\AutonomySim\Content\VehicleAdv\SUV\v1.2.0 (
-    if not defined fullPolyCar or %fullPolyCar%=="" (
+
+    if not defined fullPolyCar (
         echo "Skipping download of high-poly car asset. Default Unreal Engine vehicle will be used."
     ) else if %fullPolyCar% == y or %fullPolyCar% == yes (
+
         REM //leave some blank lines because %powershell% shows download banner at top of console
         echo.
         echo "-----------------------------------------------------------------------------------------"
@@ -181,6 +183,7 @@ if not exist Unreal\Plugins\AutonomySim\Content\VehicleAdv\SUV\v1.2.0 (
         echo " To perform the installation without these assets, run `build.cmd --no-full-poly-car`"
         echo "-----------------------------------------------------------------------------------------"
         echo.
+
         if exist tmp\suv_download rmdir tmp\suv_download /q /s
         mkdir tmp\suv_download
         REM @echo on
