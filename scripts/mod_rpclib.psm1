dbc669032ac2f41d263acd005cfc63fc341ad472
admercs <adam.michael.erickson@gmail.com>
2024-02-24 17:44:48 -0800

    update windows pwsh build

--- scripts/mod_rpclib.psm1
+++ scripts/mod_rpclib.psm1
@@ -79,7 +79,9 @@ function Build-RpcLib {
     Set-Location "${RPCLIB_PATH}\build"
     if ( $Verbose.IsPresent ) { Write-Output "Current directory: ${RPCLIB_PATH}\build" }
     # Generate RpcLib build files
-    Start-Process -FilePath 'cmake.exe' -ArgumentList "-G '${CmakeGenerator}'", '..' -Wait -NoNewWindow
+    Write-Output "Using CMake generator: ${CmakeGenerator}"
+    $ENV:CMAKE_GENERATOR = "$CmakeGenerator"
+    Start-Process -FilePath 'cmake.exe' -ArgumentList "-G `"${CmakeGenerator}"`", '..' -Wait -NoNewWindow
     # Build RpcLib
     if ( $BUILD_MODE -eq 'Release' ) {
         Start-Process -FilePath 'cmake.exe' -ArgumentList '--build', '.' -Wait -NoNewWindow
