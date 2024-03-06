943b022b688266551c3c4d56773301ffb381ba14
admercs <adam.michael.erickson@gmail.com>
2024-03-06 12:01:44 -0800

    update cmake build

--- DroneShell/include/SimpleShell.hpp
+++ DroneShell/include/SimpleShell.hpp
@@ -4,7 +4,7 @@
 #ifndef autonomylib_SimpleShell_hpp
 #define autonomylib_SimpleShell_hpp
 
-#include "common/utilsMode.hpp"
+#include "common/common_utils/StrictMode.hpp"
 STRICT_MODE_OFF
 #ifndef RPCLIB_MSGPACK
 #define RPCLIB_MSGPACK clmdep_msgpack
