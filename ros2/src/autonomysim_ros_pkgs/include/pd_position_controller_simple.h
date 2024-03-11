d02f443a6ffcabdec2749dac5f559a47061e4f5d
admercs <adam.michael.erickson@gmail.com>
2024-03-11 09:03:17 -0700

    update ros wrapper

--- ros2/src/autonomysim_ros_pkgs/include/pd_position_controller_simple.h
+++ ros2/src/autonomysim_ros_pkgs/include/pd_position_controller_simple.h
@@ -23,8 +23,8 @@ STRICT_MODE_OFF // todo what does this do?
 #include <math_common.h>
 #include <nav_msgs/msg/odometry.hpp>
 #include <rclcpp/rclcpp.hpp>
-#include <tf/LinearMath/Matrix3x3.h>
-#include <tf/LinearMath/Quaternion.h>
+#include <tf2/LinearMath/Matrix3x3.h>
+#include <tf2/LinearMath/Quaternion.h>
 #include <tf2_geometry_msgs/tf2_geometry_msgs.h>
 #include <utils.h>
 
